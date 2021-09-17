0588322e717c6244b21727e8298d89f481da2dc9
gabitheiss <t_gaaby@hotmail.com>
2021-09-17 09:29:13 -0300

    Notification Handler

---
+++ app/src/main/java/com/example/notificationhandler/di/AppModule.kt
@@ -0,0 +1,20 @@
+package com.example.notificationhandler.di
+
+import android.content.Context
+import com.example.notificationhandler.services.NotificationHandler
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+
+object AppModule {
+
+
+    @Provides
+    fun provideNotificationHandler(@ApplicationContext context: Context): NotificationHandler = NotificationHandler(context)
+
+}
--- app/src/main/java/com/example/notificationhandler/services/NotificationHandler.kt
+++ app/src/main/java/com/example/notificationhandler/services/NotificationHandler.kt
@@ -1,5 +1,56 @@
 package com.example.notificationhandler.services
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import androidx.core.app.NotificationCompat
+import com.example.notificationhandler.MainActivity
+import com.example.notificationhandler.R
+import javax.inject.Inject
 
-class NotificationHandler {
+const val CHANNEL_ID = "CHANNEL_DOCS"
+
+class NotificationHandler @Inject constructor(private val context: Context) {
+
+
+    init {
+        createNotificationChannel()
+    }
+
+    private fun createNotificationChannel() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val name = "TESTE"
+            val descriptionText = "TESTANDO"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(CHANNEL_ID, name, importance).apply {
+                description = descriptionText
+            }
+
+            val notificationManager: NotificationManager =
+                context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+            notificationManager.createNotificationChannel(channel)
+
+        }
+    }
+
+    private fun createPendingIntent(): PendingIntent {
+        val intent = Intent(context, MainActivity::class.java)
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+        return PendingIntent.getActivity(context, 0, intent, 0)
+    }
+
+    fun createNotification(title: String, message: String): Notification {
+        val builder = NotificationCompat.Builder(context, CHANNEL_ID)
+            .setSmallIcon(R.drawable.ic_launcher_background)
+            .setContentTitle(title)
+            .setContentText(message)
+            .setContentIntent(createPendingIntent())
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+        return builder.build()
+    }
+
 }
